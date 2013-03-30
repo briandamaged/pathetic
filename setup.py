a03f3a9412a9845f80590f7552dd78392968bc11
Brian Lauber <constructible.truth@gmail.com>
2013-03-30 14:26:42 -0400

    Creating initial project structure.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name = 'pathetic',
+    version = '0.0.1',
+    description = 'Path-related functions',
+    author = 'Brian Lauber',
+    author_email = 'constructible.truth@gmail.com',
+    packages = ['pathetic'],
+    install_requires = [],
+    test_suite = 'tests',
+)
+
